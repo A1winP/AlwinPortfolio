469318ceffcf948837d7dee73f51c481a1397f6c
A1winP <117494352+A1winP@users.noreply.github.com>
2023-07-31 11:50:21 -0400

    Add files via upload

---
+++ Htmlweb/action_page.php
@@ -0,0 +1,8 @@
+
+<?php
+$country = $_GET['country'];
+$firstname = $_GET['firstname'];
+$lastname = $_GET['lastname'];
+echo " $firstname , $lastname Thank you for reaching I will get back you shortly.. <br>";
+?>
+ 
